a4b6c78ee95933cae202eaeb331e44c8a52cad2d
Charles Vazquez <crv@blites.com>
2024-02-13 05:58:19 -0400

    Updated Code

--- script.js
+++ script.js
@@ -38,11 +38,11 @@ const img = document.querySelector('img');
 const profileInfo = document.querySelector('.profile-info');
 const nextBtn = document.querySelector('#next');
 
-function* createPeopleIterator () {
+function* createPeopleIterator() {
     let index = 0;
     while (true) {
         yield people[index++ % people.length];
-        console.log(index++ % people.length);
+        console.log(index);
     }
 }
 
@@ -52,8 +52,12 @@ nextBtn.addEventListener('click', () => {
     const person = iterator.next().value;
     img.src = person.imageURL;
     profileInfo.querySelector('h3').textContent = person.name;
-    profileInfo.querySelectorAll('p')[0].textContent = `${person.age} Years Old`;
-    profileInfo.querySelectorAll('p')[1].textContent = `From ${person.location}`;
+    profileInfo.querySelectorAll(
+        'p'
+    )[0].textContent = `${person.age} Years Old`;
+    profileInfo.querySelectorAll(
+        'p'
+    )[1].textContent = `From ${person.location}`;
     profileInfo.querySelectorAll('p')[2].textContent = person.looking;
 });
 
